b467177b7e108f6e967164b80ae3c21295f1ee36
Osa <osaimola@gmail.com>
2020-03-18 01:01:26 -0600

    add responsive navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import Navbar from "./components/navbar/navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>hello world</p>
-      </header>
+      <Navbar />
+      <p>hello world</p>
     </div>
   );
 }
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import logo from "../../images/logo.svg";
+import hamburger from "../../images/icon-hamburger.svg";
+import close from "../../images/icon-close.svg";
+
+const links = ["Pricing", "Products", "About Us", "Careers", "Community"];
+
+function renderLinks() {
+  return links.map(link => {
+    return (
+      <a href="#" key={link}>
+        {link}
+      </a>
+    );
+  });
+}
+
+// display or hide menu on click
+function handleHamburgerClick(iconState, setIconState) {
+  console.log("clicked");
+  iconState === "hamburger" ? setIconState("close") : setIconState("hamburger");
+}
+
+// switch between hamburger and close icons
+function renderMenuIcon(icon) {
+  switch (icon) {
+    case "hamburger":
+      return hamburger;
+    case "close":
+      return close;
+    default:
+      return hamburger;
+  }
+}
+
+function renderMenuLinks(iconState) {
+  switch (iconState) {
+    case "hamburger":
+      return;
+    case "close":
+      return <div className="nav-floating-links">{renderLinks()}</div>;
+    default:
+      return;
+  }
+}
+
+function Navbar() {
+  const [iconState, setIconState] = useState("hamburger");
+
+  return (
+    <React.Fragment>
+      <div className="nav">
+        <img alt="" src={logo} />
+        <div className="nav-links">{renderLinks()}</div>
+        <button className="nav-get-started">Get Started</button>
+
+        <img
+          src={renderMenuIcon(iconState)}
+          className="nav-toggle"
+          onClick={() => {
+            handleHamburgerClick(iconState, setIconState);
+          }}
+        />
+      </div>
+      {renderMenuLinks(iconState)}
+    </React.Fragment>
+  );
+}
+
+export default Navbar;

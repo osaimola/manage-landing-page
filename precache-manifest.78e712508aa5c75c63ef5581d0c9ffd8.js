089b0349b63ca795199b7229c45edac9db037d43
Osa <osaimola@gmail.com>
2020-03-19 22:29:08 -0600

    Updates

---
+++ precache-manifest.78e712508aa5c75c63ef5581d0c9ffd8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a92144bf8bfb9954872f5969e6ef85d",
+    "url": "/manage/index.html"
+  },
+  {
+    "revision": "e8fd2f18433bbee62b54",
+    "url": "/manage/static/css/main.03d5f2f6.chunk.css"
+  },
+  {
+    "revision": "23122d56e5eb6024460c",
+    "url": "/manage/static/js/2.6036abd2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/manage/static/js/2.6036abd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fd2f18433bbee62b54",
+    "url": "/manage/static/js/main.af0ad916.chunk.js"
+  },
+  {
+    "revision": "29f40a71eb71ea1b3b8d",
+    "url": "/manage/static/js/runtime-main.af2d0d2e.js"
+  },
+  {
+    "revision": "61d2c95a7d6f4e69ae7c4e326c309056",
+    "url": "/manage/static/media/arrow-left.61d2c95a.svg"
+  },
+  {
+    "revision": "24a05c5afdca77422b311e3cc7fefc75",
+    "url": "/manage/static/media/arrow-right.24a05c5a.svg"
+  },
+  {
+    "revision": "d022b688c087eb4bc54fa88f43ccb1be",
+    "url": "/manage/static/media/avatar-ali.d022b688.png"
+  },
+  {
+    "revision": "d591dc600c0ee916d50094bdc81d8902",
+    "url": "/manage/static/media/avatar-anisha.d591dc60.png"
+  },
+  {
+    "revision": "a2c4b4309f9936736240c6d7805c7ccf",
+    "url": "/manage/static/media/avatar-richard.a2c4b430.png"
+  },
+  {
+    "revision": "b2507cb2cb85266d8f2051f637b744aa",
+    "url": "/manage/static/media/avatar-shanai.b2507cb2.png"
+  },
+  {
+    "revision": "6e8db603e6566ef42df446715708e07e",
+    "url": "/manage/static/media/bg-simplify-desktop-top.6e8db603.svg"
+  },
+  {
+    "revision": "4249ea3f8f33574155ede22d974564a5",
+    "url": "/manage/static/media/bg-simplify-section-mobile.4249ea3f.svg"
+  },
+  {
+    "revision": "5f508d7aaa3d3a05bbd62a6c66c9c1a2",
+    "url": "/manage/static/media/bg-tablet-pattern.5f508d7a.svg"
+  },
+  {
+    "revision": "57a86b4fa04731f645568675c17138bd",
+    "url": "/manage/static/media/icon-close.57a86b4f.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/manage/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "bdd40cf9386a83e11660fab1e8fe9965",
+    "url": "/manage/static/media/icon-hamburger.bdd40cf9.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/manage/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/manage/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/manage/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/manage/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "6a53bba8095e0ddefa1791bbd03cf51f",
+    "url": "/manage/static/media/illustration-intro.6a53bba8.svg"
+  },
+  {
+    "revision": "8c964d71cac2cf24e41653c4ab3b8635",
+    "url": "/manage/static/media/logo-white.8c964d71.svg"
+  },
+  {
+    "revision": "f2cec4b29d7e7f388db937e3f5d9d873",
+    "url": "/manage/static/media/logo.f2cec4b2.svg"
+  }
+]);
